5f99cbb444df672c63437db8aa21eb003145536f
Steffew <xgenyofficial@gmail.com>
2023-12-15 15:07:21 +0100

    Rename script

--- Assets/Scripts/LayoutManager.cs
+++ Assets/Scripts/LayoutManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class SectionManager : MonoBehaviour
+public class LayoutManager : MonoBehaviour
 {
     private List<Section> sections = new List<Section>();
 
