561d83e891f49044752f059632aaad17ee0e875e
Steffew <xgenyofficial@gmail.com>
2023-12-17 02:32:29 +0100

    Fix unreachable code

--- Assets/Scripts/DropdownManager.cs
+++ Assets/Scripts/DropdownManager.cs
@@ -10,23 +10,27 @@ public class DropdownManager : MonoBehaviour
     public void SetLayoutValues(int index)
     {
         Debug.Log($"Selected layout: {index}");
-
         switch (index)
         {
             case 0: // Tiny
-                layoutManager.SetLayoutParameters(1, 2, 2, 1, 3); break;
+                layoutManager.SetLayoutParameters(1, 2, 2, 1, 3);
+                break;
 
             case 1: // Small
-                layoutManager.SetLayoutParameters(1, 2, 4, 2, 4); break;
+                layoutManager.SetLayoutParameters(1, 2, 4, 2, 4);
+                break;
 
             case 2: // Medium
-                layoutManager.SetLayoutParameters(2, 3, 5, 3, 6); break;
+                layoutManager.SetLayoutParameters(2, 3, 5, 3, 6);
+                break;
 
             case 3: // Large
-                layoutManager.SetLayoutParameters(3, 3, 6, 4, 7); break;
+                layoutManager.SetLayoutParameters(3, 3, 6, 4, 7);
+                break;
 
             case 4: // Extreme
-                layoutManager.SetLayoutParameters(4, 4, 9, 5, 9); break;
+                layoutManager.SetLayoutParameters(4, 4, 9, 5, 9);
+                break;
         }
     }
 }
