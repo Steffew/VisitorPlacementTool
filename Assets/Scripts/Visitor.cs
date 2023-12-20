0d2eb4e47ff02512e54bea3b4534d36d8aab21b7
Steffew <xgenyofficial@gmail.com>
2023-12-20 14:00:10 +0100

    Add Move methods

--- Assets/Scripts/Visitor.cs
+++ Assets/Scripts/Visitor.cs
@@ -3,4 +3,19 @@ using UnityEngine;
 public class Visitor : MonoBehaviour
 {
     public bool isAdult { get; private set; }
+
+    public Visitor(bool isAdult)
+    {
+        this.isAdult = isAdult;
+    }
+
+    public void Move(Seat seat)
+    {
+        gameObject.transform.position = seat.transform.position;
+    }
+
+    public void Move(Vector3 position)
+    {
+        gameObject.transform.position = position;
+    }
 }
