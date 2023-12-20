9976a4606057b940b40d3764d776d3e7cc47a0a8
Steffew <xgenyofficial@gmail.com>
2023-12-20 15:47:26 +0100

    Remove GUI from Seat.cs

--- Assets/Scripts/Seat.cs
+++ Assets/Scripts/Seat.cs
@@ -1,25 +1,12 @@
 using UnityEngine;
-using TMPro;
 
 public class Seat : MonoBehaviour
 {
     public int Id { get; private set; }
     public Visitor? Occupant { get; private set; }
 
-    [SerializeField]
-    private TextMeshProUGUI textStatus;
-
-    private void Start()
-    {
-    }
-
     public void SetOccupant(Visitor? occupant)
     {
         Occupant = occupant;
-    }
-
-    public void SetText(string text)
-    {
-        textStatus.text = text;
     }
 }
