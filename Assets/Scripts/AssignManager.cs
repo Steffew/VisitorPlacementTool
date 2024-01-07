fdf27d0acab8abecb7a4e095824fc016c0e04210
Steffew <xgenyofficial@gmail.com>
2024-01-07 23:55:09 +0100

    Update AssignManager.cs

--- Assets/Scripts/AssignManager.cs
+++ Assets/Scripts/AssignManager.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AssignManager : MonoBehaviour
@@ -6,33 +5,71 @@ public class AssignManager : MonoBehaviour
     [SerializeField] private LayoutManager layoutManager;
     [SerializeField] private VisitorManager visitorManager;
 
-    public void AssignVisitors() // TODO: Refactor or delete this method, as there should be a first come first serve system.
+    //public void AssignVisitors() /// TODO: Refactor or delete this method, as there should be a first come first serve system.
+    //{
+    //    List<Section> sections = layoutManager.GetSections();
+
+    //    foreach (Visitor visitor in visitorManager.GetQueue())
+    //    {
+    //        foreach (Section section in sections)
+    //        {
+    //            int firstRow = section.Seats.Count / section.Rows;
+
+    //            foreach (Seat seat in section.Seats)
+    //            {
+    //                if (seat.Occupant == null)
+    //                {
+    //                    if (seat.Id <= firstRow && !visitor.IsAdult)
+    //                    {
+    //                        seat.SetOccupant(visitor);
+    //                        visitor.MoveTo(seat);
+    //                    }
+    //                    else if (seat.Id > firstRow && visitor.IsAdult)
+    //                    {
+    //                        seat.SetOccupant(visitor);
+    //                        visitor.MoveTo(seat);
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+    public void AssignGroup(Group group)
     {
-        List<Section> sections = layoutManager.GetSections();
-
-        foreach (Visitor visitor in visitorManager.GetQueue())
+        foreach (Section section in layoutManager.GetSections())
         {
-            foreach (Section section in sections)
-            {
-                int firstRow = section.Seats.Count / section.Rows;
+            int firstRow = section.Seats.Count / section.Rows;
 
+            if (CanGroupFit(group, section) || (group.GetChildrenCount() == 0 && section.AvailableSeats.Count >= group.GetVisitors().Count))
+            {
                 foreach (Seat seat in section.Seats)
                 {
-                    if (seat.Occupant == null)
-                    {
-                        if (seat.Id <= firstRow && !visitor.IsAdult)
-                        {
-                            seat.SetOccupant(visitor);
-                            visitor.MoveTo(seat);
-                        }
-                        else if (seat.Id > firstRow && visitor.IsAdult)
-                        {
-                            seat.SetOccupant(visitor);
-                            visitor.MoveTo(seat);
-                        }
-                    }
                 }
             }
         }
+    }
+
+    public bool CanGroupFit(Group group, Section section)
+    {
+        int seatsPerRow = section.Columns;
+        int groupSize = group.GetVisitors().Count;
+
+        foreach (Seat availableSeat in section.AvailableSeats)
+        {
+            int firstAvailableIndex = section.AvailableSeats.IndexOf(availableSeat);
+
+            if (firstAvailableIndex + groupSize <= section.AvailableSeats.Count)
+            {
+                int rowStartIndex = availableSeat.Id / seatsPerRow * seatsPerRow;
+                int rowEndIndex = rowStartIndex + seatsPerRow;
+
+                if (availableSeat.Id + groupSize <= rowEndIndex)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
