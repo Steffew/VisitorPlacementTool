77fb9d34051e087d02a7d40b222dce21736ccc12
Steffew <xgenyofficial@gmail.com>
2024-01-07 16:19:45 +0100

    Update Seat.cs

--- Assets/Scripts/Seat.cs
+++ Assets/Scripts/Seat.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class Seat : MonoBehaviour
 {
     public int Id { get; private set; }
-    public Visitor? Occupant { get; private set; }
+    public Visitor Occupant { get; private set; }
 
-    public void SetOccupant(Visitor? occupant)
+    public void SetOccupant(Visitor occupant)
     {
         Occupant = occupant;
     }
