6017db6be25d8f1e8f5ebd1202c7819652566ba8
Steffew <xgenyofficial@gmail.com>
2024-01-07 17:39:33 +0100

    Add basic algorithm

--- Assets/Scripts/AssignManager.cs
+++ Assets/Scripts/AssignManager.cs
@@ -1,8 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class AssignManager : MonoBehaviour
 {
     [SerializeField] private LayoutManager layoutManager;
+
+    public void Assign(Visitor visitor)
+    {
+        List<Section> sections = layoutManager.GetSections();
+
+        foreach (Section section in sections)
+        {
+            int firstRow = section.Seats.Count / section.Rows;
+
+            foreach (Seat seat in section.Seats)
+            {
+                if (seat.Occupant == null)
+                {
+                    if (seat.Id <= firstRow && !visitor.IsAdult)
+                    {
+                        seat.SetOccupant(visitor);
+                        visitor.MoveTo(seat);
+                        break;
+                    }
+                    else if (seat.Id > firstRow && visitor.IsAdult)
+                    {
+                        seat.SetOccupant(visitor);
+                        visitor.MoveTo(seat);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
