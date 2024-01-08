6a231d29ca39e2a632ecdc4e2b1e9a0b7e136be2
Steffew <xgenyofficial@gmail.com>
2024-01-08 12:43:35 +0100

    Remove normal constructor

--- Assets/Scripts/Visitor.cs
+++ Assets/Scripts/Visitor.cs
@@ -5,7 +5,7 @@ public class Visitor : MonoBehaviour
     public int Id { get; private set; }
     public bool IsAdult { get; private set; }
 
-    public Visitor(int id, bool isAdult)
+    public void Initialize(int id, bool isAdult)
     {
         Id = id;
         IsAdult = isAdult;
