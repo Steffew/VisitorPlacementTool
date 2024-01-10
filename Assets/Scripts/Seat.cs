2d80e748f7d97e488cca567ce1901a8a9c53e671
Steffew <xgenyofficial@gmail.com>
2024-01-10 13:11:40 +0100

    Add column and row number

--- Assets/Scripts/Seat.cs
+++ Assets/Scripts/Seat.cs
@@ -5,6 +5,8 @@ public class Seat : MonoBehaviour
 {
     public int Id { get; private set; }
     public Visitor Occupant { get; private set; }
+    public int RowNumber { get; private set; }
+    public int ColumnNumber { get; private set; }
 
     public void SetId(int id)
     {
