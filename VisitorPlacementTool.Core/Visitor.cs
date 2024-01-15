831d6da7ae11ec1a55a174c72da6fab71f45737a
Steffew <xgenyofficial@gmail.com>
2024-01-15 17:40:25 +0100

    Create Visitor.cs

---
+++ VisitorPlacementTool.Core/Visitor.cs
@@ -0,0 +1,15 @@
+﻿namespace VisitorPlacementTool.Core
+{
+    public class Visitor
+    {
+        public int Id { get; private set; }
+        public bool IsAdult { get; private set; }
+
+        public Visitor(int id, bool isAdult)
+
+        {
+            Id = id;
+            IsAdult = isAdult;
+        }
+    }
+}
