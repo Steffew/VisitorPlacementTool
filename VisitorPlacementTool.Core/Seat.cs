e2adf174d40082ed6388b9ef74ab8c97b9a27536
Steffew <xgenyofficial@gmail.com>
2024-01-15 17:40:27 +0100

    Create Seat.cs

---
+++ VisitorPlacementTool.Core/Seat.cs
@@ -0,0 +1,20 @@
+﻿namespace VisitorPlacementTool.Core
+{
+    public class Seat
+    {
+        public int Id { get; private set; }
+        public Visitor? Occupant { get; private set; }
+        public int RowNumber { get; private set; }
+        public int ColumnNumber { get; private set; }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public void SetOccupant(Visitor occupant)
+        {
+            Occupant = occupant;
+        }
+    }
+}
