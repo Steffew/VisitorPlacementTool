5549f293022b6b886815ca6a77fb0f5e5fc4fb23
Steffew <xgenyofficial@gmail.com>
2024-01-15 19:39:44 +0100

    Create Layout.cs

---
+++ VisitorPlacementTool.Core/Layout.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel;
+
+namespace VisitorPlacementTool.Core
+{
+    public class Layout
+    {
+        public IEnumerable<Section> Sections { get; private set; }
+
+        public Layout()
+        {
+            Sections = new List<Section>();
+        }
+
+        public void AddSection(int rows, int columns)
+        {
+            ((List<Section>)Sections).Add(new Section(Sections.Count() + 1, rows, columns));
+        }
+
+        public void AddSection(Section section)
+        {
+            ((List<Section>)Sections).Add(section);
+        }
+
+        public void RemoveSection(Section section)
+        {
+            ((List<Section>)Sections).Remove(section);
+        }
+
+        public void ClearSections()
+        {
+            ((List<Section>)Sections).Clear();
+        }
+    }
+}
