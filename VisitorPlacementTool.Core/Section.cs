7de86d6fb639834c472cdd9d6a3e103d3762e593
Steffew <xgenyofficial@gmail.com>
2024-01-15 17:40:29 +0100

    Create Section.cs

---
+++ VisitorPlacementTool.Core/Section.cs
@@ -0,0 +1,24 @@
+namespace VisitorPlacementTool.Core
+{
+    public class Section
+    {
+        public int Id { get; private set; }
+        public List<Seat> Seats { get; private set; }
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public Section(int id, int rows, int columns)
+        {
+            Id = id;
+            Seats = new List<Seat>();
+            AvailableSeats = new List<Seat>();
+            Rows = rows;
+            Columns = columns;
+        }
+
+        public void OccupySeat(Seat seat, Visitor visitor)
+        {
+            seat.SetOccupant(visitor);
+        }
+    }
+}
