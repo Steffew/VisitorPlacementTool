1ed3632c537db6e445b31168c2a23868e14b73d4
Steffew <xgenyofficial@gmail.com>
2024-01-16 09:55:17 +0100

    Change framework

--- VisitorPlacementTool.Core/obj/Debug/net472/VisitorPlacementTool.Core.AssemblyInfo.cs
+++ VisitorPlacementTool.Core/obj/Debug/net472/VisitorPlacementTool.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisitorPlacementTool.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5583e344e7c76e9cce9a6a2007b357f86df4b5ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5014682a2f29af33412fe06002ef2455fab58f0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisitorPlacementTool.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisitorPlacementTool.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
