5583e344e7c76e9cce9a6a2007b357f86df4b5ba
Steffew <xgenyofficial@gmail.com>
2024-01-16 01:17:33 +0100

    Create LayoutManager.cs

---
+++ VisitorPlacementTool.Core/Managers/LayoutManager.cs
@@ -0,0 +1,34 @@
+﻿namespace VisitorPlacementTool.Core.Managers
+{
+    public class LayoutManager
+    {
+        public List<Section> Sections { get; private set; }
+        public List<Group> Groups { get; private set; }
+
+        public LayoutManager()
+        {
+            Sections = new List<Section>();
+            Groups = new List<Group>();
+        }
+
+        public bool CanGroupFit(Group group, Section section, bool firstRowAssignment = false)
+        {
+            int amountOfAdultsNecessary = 1;
+            int childrenCount = group.GetChildrenCount();
+            int requiredSeats = childrenCount + amountOfAdultsNecessary;
+
+            if (!firstRowAssignment)
+            {
+                int availableSeats = section.Seats.Count(seat => seat.Occupant == null);
+                return requiredSeats <= availableSeats;
+            }
+            else
+            {
+                int availableSeatsInFirstRow = section.Seats
+                    .Count(seat => seat.Occupant == null && seat.RowNumber == 1);
+
+                return requiredSeats <= availableSeatsInFirstRow;
+            }
+        }
+    }
+}
