ea8aaf12921ea058683f107b177ca9e7e643d83b
Steffew <xgenyofficial@gmail.com>
2024-01-17 15:43:08 +0100

    Add isAssigned bool to Visitor

--- VisitorPlacementTool.Core/Visitor.cs
+++ VisitorPlacementTool.Core/Visitor.cs
@@ -4,12 +4,18 @@
     {
         public int Id { get; private set; }
         public bool IsAdult { get; private set; }
+        public bool IsAssigned { get; private set; }
 
         public Visitor(int id, bool isAdult)
 
         {
             Id = id;
             IsAdult = isAdult;
+        }
+
+        public void SetIsAssigned(bool isAssigned)
+        {
+            IsAssigned = isAssigned;
         }
     }
 }
