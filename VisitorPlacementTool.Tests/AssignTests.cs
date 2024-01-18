5eed3cd862d90945832b4673d65cc87b8a2a2f2e
Steffew <xgenyofficial@gmail.com>
2024-01-18 14:03:59 +0100

    Add test

--- VisitorPlacementTool.Tests/AssignTests.cs
+++ VisitorPlacementTool.Tests/AssignTests.cs
@@ -10,38 +10,59 @@ namespace VisitorPlacementTool.Tests
         {
             // Arrange  
             var layout = new Layout();
-            layout.AddSection(new Section(id: 1, rows: 7, columns: 3));
-            layout.AddSection(new Section(id: 2, rows: 7, columns: 3));
+            layout.AddSection(new Section(id: 1, rows: 3, columns: 3));
+            layout.AddSection(new Section(id: 2, rows: 4, columns: 2));
 
             var group = new Group(id: 1);
             group.AddVisitor(new Visitor(id: 1, isAdult: true));
             group.AddVisitor(new Visitor(id: 2, isAdult: true));
             group.AddVisitor(new Visitor(id: 3, isAdult: true));
+            group.AddVisitor(new Visitor(id: 4, isAdult: true));
 
             // Act
             layout.AssignToSections(group);
 
             // Assert
-            Assert.Equal(6, layout.GetOccupiedSeatsAmount());
+            Assert.Equal(4, layout.GetOccupiedSeatsAmount());
         }
 
         [Fact]
-        public void AddAdultGroupToLayoutWithSingleSection()
+        public void TryToFitGroupInSectionWithInsufficientSpace()
         {
             // Arrange  
             var layout = new Layout();
-            layout.AddSection(new Section(id: 1, rows: 7, columns: 3));
+            Section section = new Section(id: 1, rows: 2, columns: 3);
+            layout.AddSection(section);
 
             var group = new Group(id: 1);
             group.AddVisitor(new Visitor(id: 1, isAdult: true));
-            group.AddVisitor(new Visitor(id: 2, isAdult: true));
-            group.AddVisitor(new Visitor(id: 3, isAdult: true));
+            group.AddVisitor(new Visitor(id: 2, isAdult: false));
+            group.AddVisitor(new Visitor(id: 3, isAdult: false));
 
             // Act
-            layout.AssignToSections(group);
+            section.CanGroupFit(group);
 
             // Assert
-            Assert.Equal(3, layout.GetOccupiedSeatsAmount());
+            Assert.False(section.CanGroupFit(group));
+        }
+
+        [Fact]
+        public void TryToFitGroupInSectionWithSufficientSpace()
+        {
+            // Arrange  
+            var layout = new Layout();
+            Section section = new Section(id: 1, rows: 2, columns: 3);
+            layout.AddSection(section);
+
+            var group = new Group(id: 1);
+            group.AddVisitor(new Visitor(id: 1, isAdult: true));
+            group.AddVisitor(new Visitor(id: 2, isAdult: false));
+
+            // Act
+            section.CanGroupFit(group);
+
+            // Assert
+            Assert.True(section.CanGroupFit(group));
         }
     }
 }
